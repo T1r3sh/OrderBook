59142309bf75d77240198a811c24d7d4b9faff3f
t1r3sh <sadandsand@gmail.com>
2024-02-23 21:12:11 +0300

    new approach, modify orderlist approach

--- funex/structures.py
+++ funex/structures.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, Union
+from typing import Any, Optional, Union, List
 from pydantic import BaseModel, PositiveInt, PositiveFloat
 from datetime import datetime
 import os
@@ -7,6 +7,7 @@ from enum import Enum, auto
 
 
 class OrderIdGenerator:
+
     def __init__(self):
         self.filepath = os.path.join(os.getcwd(), "current_state.txt")
         self.id_counter = self.load_state()
@@ -27,13 +28,13 @@ class OrderIdGenerator:
         return current_id
 
     def save_state(self):
-        with open(self.filepath, "w") as f:
+        with open(self.filepath, "w", encoding="utf-8") as f:
             f.write(str(self.id_counter))
 
     def load_state(self):
         if not os.path.exists(self.filepath):
             return 0
-        with open(self.filepath, "r") as f:
+        with open(self.filepath, "r", encoding="utf-8") as f:
             saved_state = f.read()
             return int(saved_state)
 
@@ -42,7 +43,7 @@ class OrderIdGenerator:
         self.save_state()
 
 
-ID_GENERATOR = OrderIdGenerator()
+# ID_GENERATOR = OrderIdGenerator()
 
 
 class OrderStatus(Enum):
@@ -81,6 +82,7 @@ class Order(BaseModel):
     listed: Optional[datetime] = None
 
     def __init__(self, **data):
+        # logging here
         super().__init__(**data)
         if self.status is None:
             self.status = OrderStatus.CREATED
@@ -93,6 +95,7 @@ class Order(BaseModel):
     def __setattr__(self, name: str, value: Any) -> None:
         if name != "updated":
             super().__setattr__("updated", datetime.now())
+        # logging here
         return super().__setattr__(name, value)
 
     def __lt__(self, other: Union[int, float, "Order"]):
@@ -125,7 +128,26 @@ class Order(BaseModel):
 
 
 class OrderList:
-    def __init__(self, order_list=None, order_type=None):
+    """
+    Order List is a collection of orders. It is used to store and manage orders.
+    Active orders are stored in a sorted list with search complexity of O(log n) and insertion complexity of O(n).
+    All orders are indexed by their id for O(1) access.
+    Active orders can be reached through bisect operations.
+    """
+
+    def __init__(
+        self,
+        order_type: OrderType,
+        order_list: Optional[List[Order]] = None,
+    ) -> None:
+        """
+        Initializes a collection of orders of a specified type. All orders within the collection must share the same order type.
+
+        :param order_type: The type of orders to be stored in the list, e.g., 'ask' or 'bid'. The specific types are defined in the OrderType enum.
+        :type order_type: OrderType
+        :param order_list: Initial list of orders to be added to the collection, defaults to None. Each order in the list is added to the collection using the 'add' method logic.
+        :type order_list: Optional[List[Order]], optional
+        """
         self.__order_list = SortedList()
         self.__ids = {}
         self.otype = order_type
@@ -133,89 +155,214 @@ class OrderList:
             for order in order_list:
                 self.add(order)
 
-    def add(self, order, tolist=True):
-        if not self.otype:
-            self.otype = order.order_type
+    def add(self, order: Order, tolist: Union[bool, str] = "auto") -> None:
+        """
+        Adds an order to the collection. The order is only added to the sorted list if it meets the criteria defined by the 'tolist' parameter. By default ('auto'), active orders are added to the sorted list.
+
+        :param order: The order to be added to the collection.
+        :type order: Order
+        :param tolist: Specifies how the order should be added to the list. 'auto' adds active orders automatically; True or 'y'/'yes' always adds; False or 'n'/'no' never adds. Defaults to 'auto'.
+        :type tolist: Union[bool, str], optional
+        :raises ValueError: If the order's type does not match the collection's type.
+        :raises ValueError: If 'tolist' is given an invalid value.
+        """
         if order.order_type != self.otype:
             raise ValueError("Order type must be the same as the list type")
-        if tolist:
+        if tolist == "auto":
+            if order.status.is_active:
+                if order.listed is None:
+                    order.listed = datetime.now()
+                self.__order_list.add(order)
+        elif tolist is True or tolist in ["y", "yes"]:
             self.__order_list.add(order)
+        elif tolist is False or tolist in ["n", "no"]:
+            pass
+        else:
+            raise ValueError("Invalid value for tolist")
         self.__ids[order.id] = order
 
-    def bisect_left(self, order, right=True):
+    def bisect_left(
+        self, order: Union[Order, float], include_right: bool = True
+    ) -> List[Order]:
+        """
+        Performs a bisect left operation on the sorted list to find the position to insert 'order' or 'price'.
+        Returns a segment of the list based on the bisect position.
+
+        :param order: Either an Order object or a float price value to perform the bisect operation.
+        :type order: Union[Order, float]
+        :param include_right: If True, returns the segment of the list from the bisect position to the end (inclusive of the position).
+                              If False, returns the segment up to the bisect position (exclusive).
+                              Defaults to True.
+        :type include_right: bool, optional
+        :return: A list of Orders filtered based on the bisect operation.
+        :rtype: List[Order]
+        """
         cid = self.__order_list.bisect_left(order)
-        if right:
+        if include_right:
             return self.__order_list[cid:]
-        else:
-            return self.__order_list[:cid]
+        return self.__order_list[:cid]
 
-    def bisect_right(self, order, left=True):
+    def bisect_right(
+        self, order: Union[Order, float], include_left: bool = True
+    ) -> List[Order]:
+        """
+        Performs a bisect right operation on the sorted list to find the position to insert 'order' or 'price'.
+        Returns a segment of the list based on the bisect position.
+
+        :param order: Either an Order object or a float price value to perform the bisect operation.
+        :type order: Union[Order, float]
+        :param include_left: If True, returns the segment of the list up to the bisect position (inclusive).
+                             If False, returns the segment of the list from the bisect position to the end (exclusive).
+                             Defaults to True.
+        :type include_left: bool, optional
+        :return: A list of Orders filtered based on the bisect operation.
+        :rtype: List[Order]
+        """
         cid = self.__order_list.bisect_right(order)
-        if left:
+        if include_left:
             return self.__order_list[:cid]
-        else:
-            return self.__order_list[cid:]
+        return self.__order_list[cid:]
 
-    def relist(self, order=None, order_id=None):
-        if not order and order_id is None:
-            raise ValueError("Order or order_id must be provided")
-        if order_id and order:
-            print("Warning: both order and order_id are provided. Using order_id.")
-        if order_id:
-            pass
-        elif order:
+    def modify(
+        self,
+        order: Union[Order, int],
+        price: Optional[float] = None,
+        volume: Optional[int] = None,
+        order_status: Optional[OrderStatus] = None,
+        force_relist: bool = False,
+    ) -> None:
+        """
+        Modify an order based on provided parameters. Updates the order's price, volume, and/or status.
+        Conditions for relisting in the sorted list:
+        - If the order's status changes from active to non-active, it's removed.
+        - If the order's status changes from non-active to active, it's added.
+        - If the price changes and the order is active, it's re-added to update its position.
+        - Force relist if specified, regardless of other changes, but only if the order remains or becomes active.
+
+        :param order: The order or order ID to be modified.
+        :param price: The new price of the order, defaults to None.
+        :param volume: The new volume of the order, defaults to None.
+        :param order_status: The new status of the order, defaults to None.
+        :param force_relist: Forces the order to be re-added to the list, defaults to False.
+        :raises ValueError: If the order or order ID is invalid or not found.
+        """
+        if isinstance(order, int):
+            order = self.__ids.get(order)
+            if order is None:
+                raise ValueError("Provided order ID not found")
+        elif not isinstance(order, Order):
+            raise ValueError("Invalid order type")
+
+        needs_relist = False
+
+        if volume is not None:
+            order.volume = volume
+
+        if order_status is not None and order_status != order.status:
+            pre_status = order.status
+            order.status = order_status
+
+            if not order.status.is_active:
+                if pre_status.is_active:
+                    self.__order_list.remove(order)
+            else:  # status is active
+                needs_relist = True
+
+        # Handle price change
+        if price is not None and price != order.price:
+            order.price = price
+            if order.status.is_active:
+                needs_relist = True
+
+        # Relist if necessary
+        if (needs_relist or force_relist) and order.status.is_active:
+            # Ensure order is not already in the list due to prior operations
+            if order in self.__order_list:
+                self.__order_list.remove(order)
+            order.listed = datetime.now()
+            self.__order_list.add(order)
+
+    def relist(self, order: Union[Order, int]) -> None:
+        """
+        Relist an order that is not active.
+
+        :param order: order to be relisted, it can be either an order or an order id
+        :type order: Union[Order, int]
+        :raises ValueError: if provided order is invalid type
+        :raises ValueError: if order is not found
+        :raises ValueError: if order is active
+        """
+        if isinstance(order, int):
+            order_id = order
+        elif isinstance(order, Order):
             order_id = order.id
+        else:
+            raise ValueError("Invalid order")
         try:
             order = self.__ids[order_id]
-        except KeyError:
-            raise ValueError("Order with provided id not found")
-        if order.status == OrderStatus.CANCELLED or order.status == OrderStatus.EXPIRED:
-            order.listed = time.time()
-            self.__order_list.add(order)
-        else:
+        except KeyError as e:
+            raise ValueError("Order with provided id not found") from e
+        if order.status.is_active:
             raise ValueError("Order is active.")
+        else:
+            order.listed = datetime.now()
+            order.status = OrderStatus.RESTORED
+            self.__order_list.add(order)
 
-    def remove(self, order=None, order_id=None):
-        if not order and order_id is None:
-            raise ValueError("Order or order_id must be provided")
-        if order_id and order:
-            print("Warning: both order and order_id are provided. Using order_id.")
-        if order_id:
-            pass
-        elif order:
+    def remove(self, order: Union[Order, int]) -> None:
+        """
+        Remove an order from the collection.
+
+        :param order: order to be removed, it can be either an order or an order id
+        :type order: Union[Order, int]
+        :raises ValueError: if provided order is invalid type
+        :raises ValueError: if order is not found
+        """
+        if isinstance(order, int):
+            order_id = order
+        elif isinstance(order, Order):
             order_id = order.id
+        else:
+            raise ValueError("Invalid order")
         try:
+            order = self.__ids[order_id]
+        except KeyError as e:
+            raise ValueError("Provided order not found") from e
+        if order.status.is_active:
             self.__order_list.remove(order)
-        except ValueError:
-            pass
         del self.__ids[order_id]
 
-    def unlist(self, order=None, order_id=None, status=OrderStatus.EXPIRED):
-        if not order and order_id is None:
-            raise ValueError("Order or order_id must be provided")
-        if order_id and order:
-            print("Warning: both order and order_id are provided. Using order_id.")
-        if order_id:
-            pass
-        elif order:
+    def unlist(self, order: Union[Order, int], status=OrderStatus.EXPIRED) -> None:
+        """
+        Unlist an active order.
+
+        :param order: order to be unlisted, it can be either an order or an order id
+        :type order: Union[Order, int]
+        :param status: status to change, defaults to OrderStatus.EXPIRED
+        :type status: _type_, optional
+        :raises ValueError: _description_
+        :raises ValueError: _description_
+        :raises ValueError: _description_
+        """
+        if isinstance(order, int):
+            order_id = order
+        elif isinstance(order, Order):
             order_id = order.id
-        order = self.__ids[order_id]
-        if (
-            order.status == OrderStatus.CANCELLED
-            or order.status == OrderStatus.EXPIRED
-            or order.status == OrderStatus.FILLED
-        ):
+        else:
+            raise ValueError("Invalid order")
+        try:
+            order = self.__ids[order_id]
+        except KeyError as e:
+            raise ValueError("Provided order not found") from e
+        if not order.status.is_active:
             raise ValueError("Order is not active.")
         else:
             order.status = status
             self.__order_list.remove(order)
 
-    def clear(self):
+    def clear(self) -> None:
         self.__order_list.clear()
         self.__ids.clear()
-
-    def get(self, order_id):
-        return self.__ids[order_id]
 
     def __getitem__(self, order_id):
         return self.__ids[order_id]
@@ -224,7 +371,7 @@ class OrderList:
         return iter(self.__order_list)
 
     def __len__(self):
-        return len(self.__order_list)
+        return len(self.__ids)
 
     def __repr__(self):
         return str(self.__order_list)
